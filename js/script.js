75e48c35cd01566e79624346bba1ec4ed0f3727f
acasax <acasax@gmail.com>
2023-01-02 14:17:59 +0100

    fix courosaul

--- js/script.js
+++ js/script.js
@@ -718,32 +718,32 @@ When document is Scrollig, do
 
 const gap = 16;
 
-const carousel = document.getElementById('carousel'),
-    content = document.getElementById('content'),
-    next = document.getElementById('next'),
-    prev = document.getElementById('prev');
+// const carousel = document.getElementById('carousel'),
+//     content = document.getElementById('content'),
+//     next = document.getElementById('next'),
+//     prev = document.getElementById('prev');
 
-next.addEventListener('click', (e) => {
-    carousel.scrollBy(width + gap, 0);
-    if (carousel.scrollWidth !== 0) {
-        prev.style.display = 'flex';
-    }
-    if (content.scrollWidth - width - gap <= carousel.scrollLeft + width) {
-        next.style.display = 'none';
-    }
-});
-prev.addEventListener('click', (e) => {
-    carousel.scrollBy(-(width + gap), 0);
-    if (carousel.scrollLeft - width - gap <= 0) {
-        prev.style.display = 'none';
-    }
-    if (!content.scrollWidth - width - gap <= carousel.scrollLeft + width) {
-        next.style.display = 'flex';
-    }
-});
+// next.addEventListener('click', (e) => {
+//     carousel.scrollBy(width + gap, 0);
+//     if (carousel.scrollWidth !== 0) {
+//         prev.style.display = 'flex';
+//     }
+//     if (content.scrollWidth - width - gap <= carousel.scrollLeft + width) {
+//         next.style.display = 'none';
+//     }
+// });
+// prev.addEventListener('click', (e) => {
+//     carousel.scrollBy(-(width + gap), 0);
+//     if (carousel.scrollLeft - width - gap <= 0) {
+//         prev.style.display = 'none';
+//     }
+//     if (!content.scrollWidth - width - gap <= carousel.scrollLeft + width) {
+//         next.style.display = 'flex';
+//     }
+// });
 
-let width = carousel.offsetWidth;
-window.addEventListener('resize', (e) => (width = carousel.offsetWidth));
+// let width = carousel.offsetWidth;
+// window.addEventListener('resize', (e) => (width = carousel.offsetWidth));
 
 $(function () {
     if ($('.owl-2').length > 0) {
